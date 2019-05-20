8a9b41648c3981202a59bb344869c36bf03d2f60
iamatran <iamatran@hotmail.com>
2019-05-19 21:55:24 -0500

    Added Tests for game views
    Completed issue #3

--- spaceman/tests/test_game_views.py
+++ spaceman/tests/test_game_views.py
@@ -31,7 +31,7 @@ class GameApiViewTests( TestCase ):
 
         self.request_factory = APIRequestFactory()
         self.mock_get_request = self.request_factory.get('dummy')
-
+        self.mock_put_request = self.request_factory.put('dummy')
 
     ### POST (create game) view
     def test_game_view_should_create_new_game_on_POST( self ):
@@ -72,4 +72,23 @@ class GameApiViewTests( TestCase ):
     # TODO: Add tests for Getting a game's solution
     # HINT: remember the `setUp` fixture that is in this test class, 
     #   it constructs things that might be useful
+
+    def test_game_view_give_404_when_id_not_found( self ):
+        with patch.object( Game.objects, 'get' ) as mock_get:
+            mock_get.side_effect = Game.DoesNotExist
+
+            response = game_view( self.mock_put_request, 25 )
+            self.assertEqual(response.status_code, 404)
+
+    def test_game_view_give_game_solution_on_get (self):
+        with patch.object( Game.objects, 'get' ) as mock_get:
+            mock_get.return_value = self.mock_game
+
+            response = game_solution( self.mock_get_request, 25)
+
+            mock_get.assert_called_with ( pk = 25 ) 
+            self.assertEqual (response.status_code, 200)
+
+            self.expected_game_solution = { 'solution' : 'TESTWORD'}
+            self.assertDictEqual( response.data, self.expected_game_solution)
 
